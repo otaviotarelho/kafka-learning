2fe197fe3413d0bb6b3e8882adaa7fa1b599fe8d
otaviotarelho <otarelho@ucdavis.edu>
2018-12-24 00:19:22 -0200

    Lecture 45 - First Producer Code

--- src/main/java/edu/otaviotarelho/tutorialone/ProducerDemo.java
+++ src/main/java/edu/otaviotarelho/tutorialone/ProducerDemo.java
@@ -1,8 +1,34 @@
 package edu.otaviotarelho.tutorialone;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
+
+        //create properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Start kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hallo world");
+
+        //send
+        producer.send(record);
+
+        //flush and close producer
+        producer.flush();
+        producer.close();
 
     }
 }
