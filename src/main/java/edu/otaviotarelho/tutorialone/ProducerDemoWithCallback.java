30bbb0a93938383603fb779967bcc10768063a2f
otaviotarelho <otarelho@ucdavis.edu>
2018-12-24 00:33:55 -0200

    Lecture 46 - Producer with callback

---
+++ src/main/java/edu/otaviotarelho/tutorialone/ProducerDemoWithCallback.java
@@ -0,0 +1,53 @@
+package edu.otaviotarelho.tutorialone;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+    public static void main(String[] args) {
+
+        //create properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Start kafka producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> record;
+
+
+        //send
+        for(int i = 0; i < 10; i++){
+            //create producer record
+            record = new ProducerRecord<String, String>("first_topic", "hallo world = " + i);
+
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if(e == null) {
+                        log.info("Received Matadata: \n Topic :"  + recordMetadata.topic() +
+                                "\n Partition: " + recordMetadata.partition() +
+                                "\n Offset: " + recordMetadata.offset() +
+                                "\n Timestemp " + recordMetadata.timestamp()
+                        );
+
+                    } else {
+                        log.info("Error in production", e);
+                    }
+                }
+            });
+        }
+
+        //flush and close producer
+        producer.flush();
+        producer.close();
+
+    }
+}
